2c852430a9af7b44a5e53555a38dd992c2c2e6de
Sarin CR <sarincrg@gmail.com>
2019-08-18 17:59:11 +0530

    LCD

---
+++ 12.LCD_Display/Lcd.asm
@@ -0,0 +1,26 @@
+
+_main:
+
+;Lcd.c,15 :: 		void main()
+;Lcd.c,17 :: 		Lcd_Init(); // Initialize LCD
+	CALL       _Lcd_Init+0
+;Lcd.c,18 :: 		Lcd_Cmd(_LCD_CLEAR); // Clear display
+	MOVLW      1
+	MOVWF      FARG_Lcd_Cmd_out_char+0
+	CALL       _Lcd_Cmd+0
+;Lcd.c,19 :: 		Lcd_Cmd(_LCD_CURSOR_OFF); // Cursor off
+	MOVLW      12
+	MOVWF      FARG_Lcd_Cmd_out_char+0
+	CALL       _Lcd_Cmd+0
+;Lcd.c,20 :: 		Lcd_Out(1,1,"Hello World");//Write text'Hello World' in first row
+	MOVLW      1
+	MOVWF      FARG_Lcd_Out_row+0
+	MOVLW      1
+	MOVWF      FARG_Lcd_Out_column+0
+	MOVLW      ?lstr1_Lcd+0
+	MOVWF      FARG_Lcd_Out_text+0
+	CALL       _Lcd_Out+0
+;Lcd.c,21 :: 		}
+L_end_main:
+	GOTO       $+0
+; end of _main
---
+++ 12.LCD_Display/Lcd.c
@@ -0,0 +1,21 @@
+// LCD module connections
+sbit LCD_RS at RC2_bit;
+sbit LCD_EN at RC3_bit;
+sbit LCD_D4 at RC4_bit;
+sbit LCD_D5 at RC5_bit;
+sbit LCD_D6 at RC6_bit;
+sbit LCD_D7 at RC7_bit;
+sbit LCD_RS_Direction at TRISC2_bit;
+sbit LCD_EN_Direction at TRISC3_bit;
+sbit LCD_D4_Direction at TRISC4_bit;
+sbit LCD_D5_Direction at TRISC5_bit;
+sbit LCD_D6_Direction at TRISC6_bit;
+sbit LCD_D7_Direction at TRISC7_bit;
+// End LCD module connections
+void main()
+{
+ Lcd_Init(); // Initialize LCD
+ Lcd_Cmd(_LCD_CLEAR); // Clear display
+ Lcd_Cmd(_LCD_CURSOR_OFF); // Cursor off
+ Lcd_Out(1,1,"Hello World");//Write text'Hello World' in first row
+}
